a511330aa807a9e9890ecbec117a96489b199612
zhonghx <zhonghx@raiyee.com>
2020-01-19 18:02:30 +0800

    '1'

--- src/main/java/com/zongzi/smsserver/service/SmsProvider.java
+++ src/main/java/com/zongzi/smsserver/service/SmsProvider.java
@@ -1,4 +1,36 @@
 package com.zongzi.smsserver.service;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SmsProvider {
+
+    // 阿里云短信地址
+    private String baseUrl = "dysmsapi.aliyuncs.com";
+    //
+    private String accessKeyId = "LTAI4FvJuhsCLM4NvbBU4NU7";
+    private String accessSecret = "RF5TX5Cuh7bf8GQeDe9pJ2BF6n1Qjc";
+
+    /**
+     * 请求结构：https://dysmsapi.aliyuncs.com/?Action=SendSms&<公共请求参数>
+     *
+     */
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
